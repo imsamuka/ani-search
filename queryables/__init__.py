a356396fc1e39b6c33c52bf5d5927eee7125fbcb
imsamuka <imsamuka@gmail.com>
2021-11-23 14:42:34 -0300

    create info_anime queryable

--- queryables/__init__.py
+++ queryables/__init__.py
@@ -3,10 +3,11 @@ from queryables.queryable import Queryable
 from queryables.uniotaku import Uniotaku
 from queryables.mdan import MDAN
 from queryables.animeNSK import AnimeNSK_Packs
+from queryables.info_anime import Info_Anime
 
 from enum import Enum
 
-queryables_list = (Uniotaku, MDAN, AnimeNSK_Packs)
+queryables_list = (Uniotaku, MDAN, AnimeNSK_Packs, Info_Anime)
 queryables_dict = {q.__name__: q for q in queryables_list}
 queryables_enum = Enum(
     'Queryables', {str(i): q.__name__ for i, q in enumerate(queryables_list)})
---
+++ queryables/info_anime.py
@@ -0,0 +1,66 @@
+from queryables.queryable import *
+
+
+class Info_Anime(Queryable):
+
+    END_POINT = "https://www.infoanime.com.br/"
+
+    @classmethod
+    def make_request(cls, query: str, all_pages=False, page=0, length=25, **kwargs) -> dict:
+        entries = kwargs.get("entries", [])
+        start = page * length
+        url = cls.END_POINT + "/listageral"
+
+        res = requests.get(url)
+        content = (res and (res.status_code == 200)
+                   and res.content) or "<html></html>"
+        soup = BeautifulSoup(content, 'html.parser')
+
+        ul = soup.find(id="myUL")
+        all_links = ul.find_all("a", href=re.compile(r"^dados\?obra="))
+        # TODO: Cache this
+        link_gen = ((get_body(a), cls.END_POINT + get_href(a))
+                    for a in all_links)
+
+        filtered = tuple(filter(lambda e: query in e[0].lower(), link_gen))
+        entries.extend(filtered[start:]
+                       if all_pages else filtered[start:start+length])
+
+        total = len(filtered)
+        showing = len(entries)
+        remaining = total - (start + showing)
+        if remaining < 0:
+            remaining = 0
+
+        return {
+            "entries": entries,
+            "start": start,
+            "showing": showing,
+            "remaining": remaining,
+            "total": total,
+        }
+
+    @classmethod
+    def parse_entries(cls, entries: list) -> list[dict]:
+        new_entries = []
+        for cell in entries:
+            new_entries.append({
+                "title": cell[0],
+                "page": cell[1],
+            })
+        return new_entries
+
+    @classmethod
+    def make_table(cls, data: dict) -> Table:
+        t = cls._Table(data)
+
+        t.add_column("Title")
+        t.add_column("Page Link", style="dim")
+
+        for cell in data['entries']:
+            t.add_row(
+                cell['title'],
+                cell['page'],
+            )
+
+        return t
