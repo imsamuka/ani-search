7fb7fe13010ebca4cf4fafcbc62219376dfcabc7
imsamuka <imsamuka@gmail.com>
2021-11-23 17:06:25 -0300

    add json cache functionality

--- queryables/queryable.py
+++ queryables/queryable.py
@@ -1,11 +1,16 @@
 import logging
 import requests
 import re
+import json
+from os.path import dirname, realpath
 from bs4 import BeautifulSoup, Tag
 from rich.table import Table
 from time import sleep
 from functools import reduce
 from math import ceil
+
+CACHE_FILE = dirname(dirname(realpath(__file__))) + "/cache.json"
+cache = None
 
 
 def with_style(s, style): return f"[{style}]{s}[/]"
@@ -18,6 +23,50 @@ def get_body(tag): return str((tag and tag.string) or "").strip()
 
 
 def as_int(s): return int(re.sub(r"\D*", '', s) or 0)
+
+
+def save_cache():
+    global cache
+
+    if cache is None or not cache:
+        logging.info("Tried to save empty Cache")
+        return
+
+    try:
+        with open(CACHE_FILE, "w") as f:
+            logging.debug(f"Saving cache: {cache}")
+            json.dump(cache, f, indent=2)
+            logging.info(f"Saved cache")
+    except Exception as e:
+        logging.error(f"Exception ocurred while writing cache file: {e}")
+
+
+def open_cache(force: bool = False):
+    global cache
+
+    if not force and cache is not None:
+        logging.info("Cache was opened already")
+        return
+
+    try:
+        with open(CACHE_FILE, "r") as f:
+            data = json.load(f)
+
+            if isinstance(data, dict):
+                cache = data
+            else:
+                logging.warning(
+                    f"Cache file is {type(data)} instead of {dict}. Ignoring it")
+    except FileNotFoundError as e:
+        pass
+    except json.JSONDecodeError as e:
+        logging.error(f"Couldn't decode cache file: {e}")
+    except Exception as e:
+        logging.error(f"Exception ocurred while reading cache file: {e}")
+
+    if cache is None:
+        logging.info("Creating empty Cache")
+        cache = {}
 
 
 class MissingCookiesError(Exception):
@@ -66,6 +115,31 @@ class Queryable:
         if expired:
             _error = f"{cls.NAME()} - Cookies expired or invalid: page is requiring login.\nLogin again at {cls.END_POINT}"
             raise ExpiredCookiesError(_error)
+
+    @classmethod
+    def write_cache(cls, key, value):
+        global cache
+        open_cache()
+
+        logging.info(f"Writing cache['{cls.__name__}']['{key}']")
+        cache.setdefault(cls.__name__, {})[key] = value
+
+        save_cache()
+
+    @classmethod
+    def read_cache(cls, key):
+        global cache
+        open_cache()
+
+        if not cache:
+            logging.info("Cache is empty")
+            return
+
+        if cache and cls.__name__ in cache:
+            logging.info(f"Getting cache['{cls.__name__}']['{key}']")
+            return cache.get(cls.__name__, {}).get(key)
+
+        logging.info(f"cache['{cls.__name__}'] doesn't exist")
 
     @classmethod
     def _Table(cls, data: dict) -> Table:
