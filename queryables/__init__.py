6b07f1fc99bf4d35adba86b90e800f9bba9d28de
imsamuka <imsamuka@gmail.com>
2021-11-25 00:34:11 -0300

    change queryables order - cached ones first

--- queryables/__init__.py
+++ queryables/__init__.py
@@ -1,14 +1,14 @@
 from queryables.queryable import Queryable
 
+from queryables.info_anime import Info_Anime
+from queryables.animeNSK import AnimeNSK_Packs, AnimeNSK_Torrent
 from queryables.uniotaku import Uniotaku
 from queryables.mdan import MDAN
-from queryables.animeNSK import AnimeNSK_Packs, AnimeNSK_Torrent
-from queryables.info_anime import Info_Anime
 
 from enum import Enum
 
-queryables_list = (Uniotaku, MDAN, AnimeNSK_Packs,
-                   AnimeNSK_Torrent, Info_Anime)
+queryables_list = (Info_Anime, AnimeNSK_Packs,
+                   AnimeNSK_Torrent, Uniotaku, MDAN)
 queryables_dict = {q.__name__: q for q in queryables_list}
 queryables_enum = Enum(
     'Queryables', {str(i): q.__name__ for i, q in enumerate(queryables_list)})
